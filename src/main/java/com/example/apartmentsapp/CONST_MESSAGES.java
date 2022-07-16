87563572c0ff1b0ea0dfd6e517db46f41b747e62
Edvard Krainiy <ekrayniy@inbox.ru>
2022-07-16 23:29:52 +0300

    Minor fix.

--- src/main/java/com/example/apartmentsapp/CONST_MESSAGES.java
+++ src/main/java/com/example/apartmentsapp/CONST_MESSAGES.java
@@ -1,7 +1,7 @@
 package com.example.apartmentsapp;
 
 public class CONST_MESSAGES {
-  public static final String NEW_FLAT_MESSAGE_CATALOG = "New flat Catalog! Price: %s USD, URL: %s Ring this!";
+  public static final String NEW_FLAT_MESSAGE_CATALOG = "CATALOG New flat! Price: %s$, URL: %s Ring this!";
   public static final String NEW_FLAT_MESSAGE_KUFAR =
-      "New flat Kufar! Price: %s USD, %s BYN, URL: %s Ring this!";
+      "New flat Kufar! Price: %s$, %s BYN, URL: %s Ring this!";
 }
